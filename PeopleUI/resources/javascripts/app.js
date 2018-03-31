804638bfb304ae7a24bc5b90a62b1e10d2762269
Ido Shtivi <idoshtivi@gmail.com>
2018-03-31 04:50:07 +0300

    people ui basic setup

---
+++ PeopleUI/resources/javascripts/app.js
@@ -0,0 +1,38 @@
+var app = angular.module("people", ['ngMaterial', 'ngRoute', 'peopleServices']);
+
+app.config(function($routeProvider, $mdThemingProvider) {
+    // Theming
+    var darkBlueGrey = $mdThemingProvider.extendPalette("blue-grey", {
+        '800': "#022C36",
+        'contrastLightColors': ['800'],
+        'contrastDefaultColor': 'dark'
+    });
+
+    $mdThemingProvider.definePalette('dark-blue-grey', darkBlueGrey);
+
+    $mdThemingProvider.theme("WhosHome")
+        .primaryPalette('dark-blue-grey')
+        .accentPalette("amber")
+        .warnPalette("red")
+        .backgroundPalette("grey");
+
+    $mdThemingProvider.setDefaultTheme("WhosHome");    
+
+    // Routing
+    // $routeProvider.when("/home", {
+    //     templateUrl: "./views/home/home.view.html",
+    //     controller: "homeCtrl"
+    // }).when("/history", {
+    //     templateUrl: "./views/history/history.view.html",
+    //     controller: "historyCtrl"
+    // }).when("/sensors", {
+    //     templateUrl: "./views/sensors/sensors.view.html",
+    //     controller: "sensorsCtrl"
+    // }).otherwise({
+    //     redirectTo: "/home"
+    // })
+})
+
+app.run(function() {
+    
+})
---
+++ PeopleUI/resources/javascripts/services/services.js
@@ -0,0 +1,4 @@
+var services = angular.module("peopleServices", []);
+
+services.constant('serverUrl', 'http://localhost:5020');
+services.constant("notificationsServiceUrl", "ws://localhost:5001")
