edac99d2b72c658aa0b1217ad4e63fdee832fd43
Ido Shtivi <idoshtivi@gmail.com>
2018-04-20 19:22:19 +0300

    upload picture dialog sends files

--- PeopleUI/resources/views/upload-picture-dialog/upload-picture.ctrl.js
+++ PeopleUI/resources/views/upload-picture-dialog/upload-picture.ctrl.js
@@ -1,3 +1,16 @@
-app.controller("uploadPictureCtrl", function($scope, $mdToast, picturesService, peopleService) {
-    
+app.controller("uploadPictureCtrl", function($scope, $mdToast, picturesService, peopleService, serverUrl) {
+    // Init
+    $scope.init = () => {
+        // Wait for the dropzone div to load, them create the drop zone object
+        angular.element(document.getElementById('#dropzone')).ready(() => {
+            $scope.dropzone = new Dropzone("div#dropzone", {
+                url: serverUrl + '/api/pictures/upload',
+                init: () => {
+                    // INIT DROPZONE
+                },
+                maxFiles: 1,
+                dictDefaultMessage: "Drop your image here to start uploading and proccessing it"
+            })
+        })
+    };
 })
