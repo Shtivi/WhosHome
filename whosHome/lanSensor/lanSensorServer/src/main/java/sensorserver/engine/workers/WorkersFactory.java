1c8024c997193f2d5c7d01b3b2f892552519d356
Ido Shtivi <idoshtivi@gmail.com>
2019-03-06 23:12:01 +0200

    Back to NetScannerV1

--- whosHome/lanSensor/lanSensorServer/src/main/java/sensorserver/engine/workers/WorkersFactory.java
+++ whosHome/lanSensor/lanSensorServer/src/main/java/sensorserver/engine/workers/WorkersFactory.java
@@ -4,7 +4,7 @@ import sensorserver.engine.tasks.ScanningTask;
 
 public class WorkersFactory implements IWorkersFactory<Runnable, ScanningTask> {
     @Override
-    public NetScannerV2 create(IScannerListener listener, ScanningTask task) {
-        return new NetScannerV2(listener, task);
+    public Runnable create(IScannerListener listener, ScanningTask task) {
+        return new NetScanner(listener, task);
     }
 }
